abf82330a48b2d1c0525469c0b7ebea2f31515f8
rkdwldk24 <rkdwldk16@gmail.com>
2021-10-10 23:55:46 +0900

    feat: PW/email check, function about form / fix: delete yarn.lock / style: change styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import GlobalStyle from './styles/globalStyles';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Join from './components/join/JoinPresenter';
+// import GlobalStyle from './styles/globalStyles';
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalStyle />
-      <Switch></Switch>
-    </BrowserRouter>
+    <Join/>
   );
 }
 
--- src/components/join/joinPresenter.jsx
+++ src/components/join/joinPresenter.jsx
@@ -1,22 +1,29 @@
-import React,{useState} from 'react';
+import React,{useState, useRef} from 'react';
 import styled from 'styled-components';
-import DaumPostCode from 'react-daum-postcode';
+import DaumPostCode from 'react-daum-postcode'; // 우편주소 검색 리액트 라이브러리
 
 const Join = () => {
   const [popup, setPopup] = useState(false);
-  const [address, setAddress] = useState('');
-  const [zoneCode, setZoneCode] = useState('');
+  const [address, setAddress] = useState(''); //주소 state
+  const [zoneCode, setZoneCode] = useState(''); // 우편번호 state
+  const [passwordType, setPasswordType] = useState({  //password type 변경용 state
+    type: 'password',
+    visible: false,
+  });
 
+  const buttonRef = useRef();
+
+  //우편번호 검색페이지 주소생성함수
   const handleAddress = data => {
-    let AllAddress = data.address;    //주소 변수
-    let zonecode = data.zonecode; 
-    let extraAddress = '';     //()안 참고항목 변수
+    let AllAddress = data.address;  
+    let zonecode = data.zonecode; // 우편번호
+    let extraAddress = ''; // 전체 주소(상세주소 제외)
     
     if (data.addressType === 'R') {     
       if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {  
         extraAddress += data.bname;
       }
-      if (data.buildingName !== '') {   // 건물명이 있고
+      if (data.buildingName !== '') {   // 건물명
         extraAddress +=
           extraAddress !== '' ? ', ' + data.buildingName : data.buildingName;
       }
@@ -26,64 +33,96 @@ const Join = () => {
     setZoneCode(zonecode);
    };
 
+   //우편번호 검색 페이지 관련 CSS
    const postCodeStyle = {
     position: 'absolute',
-    top: 100,
+    top: '50%',
     left: 'width/2',
-    width: '400px',
+    width: '500px',
     height: '500px',
     display: 'block',
     backgroundColor: 'rgba(0, 0, 0, 0.4)',
     border: '1px black solid',
+    zIndex: 500,
   };
+
+  const handleCheck = e => {
+    const div = document.createElement('div');
+    div.innerHTML = `<div>중복되는 이메일입니다.</div>`;
+    div.style.textAlign = 'center';
+    div.style.color = 'red';
+    div.style.marginTop = '15px';
+    e.target.parentNode.appendChild(div);
+  }
+
+  //password type 변경하는 함수
+  const handlePasswordType = e => {
+      setPasswordType(() => {
+        if (!passwordType.visible) {
+            return { type: 'text', visible: true };
+        }
+        return { type: 'password', visible: false };
+    })
+  }
+
+  // form 기본기능 방지
+  const handleCreate = e => {
+    e.preventDefault();
+  }
 
   return(
     <Container>
       <JoinWapper>
-        <h1>회원가입</h1>
-        <InputList>
-          <InputField>
+        <JoinTitle>회원가입</JoinTitle>
+        <InputForm onSubmit={handleCreate}>
+          <InputWapper>
             <InputTitle>이메일</InputTitle>
-            <Input type="text" placeholder="email을 입력해주세요."/>
-            <InnerButton>중복확인</InnerButton>
-            <AlertLine>사용할 수 있는 이메일입니다.</AlertLine>
-            <AlertLine>사용할 수 없는 이메일입니다.</AlertLine>
-          </InputField>
-          <InputField>
+            <EmailInput type="text" placeholder="email을 입력해주세요."/>
+            <CheckButton ref={buttonRef} onClick={handleCheck}>중복확인</CheckButton>
+          </InputWapper>
+          <InputWapper>
             <InputTitle>닉네임</InputTitle>
             <Input type="text" max-length="10" placeholder="닉네임을 입력해주세요."/>
-          </InputField>
-          <InputField>
+          </InputWapper>
+            <AlertLine>사용할 수 있는 이메일입니다.</AlertLine>
+            <AlertLine>사용할 수 없는 이메일입니다.</AlertLine>
+          <InputWapper>
             <InputTitle>비밀번호</InputTitle>
-            <Input type="text" max-length="16" placeholder="비밀번호를 입력해주세요."/>
-          </InputField>
-          <InputField>
-            <InputTitle>비밀번호 확인</InputTitle>
-            <Input type="text" max-length="16" placeholder="비밀번호를 재입력해주세요."/>
-          </InputField>
-          <InputField>
+            <PWInput minLength='8' maxLength='16' type={passwordType.type} placeholder="비밀번호를 입력해주세요."/>
+            <CheckPW onClick={handlePasswordType}>
+            	{passwordType.visible ? <span>비밀번호 숨기기</span> : <span>비밀번호 보기</span>}
+            </CheckPW> 
+          </InputWapper> 
+          <InputWapper>
+            <InputTitle>재확인</InputTitle>
+            <PWInput minLength='8' maxLength="16" type={passwordType.type} placeholder="비밀번호를 재입력해주세요."/>
+            <CheckPW onClick={handlePasswordType}>
+            	{passwordType.visible ? <span>비밀번호 숨기기</span> : <span>비밀번호 보기</span>}
+            </CheckPW> 
+          </InputWapper> 
+          <InputWapper>
             <InputTitle>전화번호</InputTitle>
             <Input type="number" placeholder="전화번호를 입력해주세요."/>
-          </InputField>
-          <InputField>
+          </InputWapper> 
+          <InputWapper>
             <InputTitle>우편번호</InputTitle>
-            <Input value={zoneCode} type="text" placeholder="우편번호를 입력해주세요."/>
-            <InnerButton onClick={ () => setPopup(true) }>우편번호 검색</InnerButton>
+            <ZoneCodeInput value={zoneCode} type="text" placeholder="우편번호를 입력해주세요."/>
+            <SearchButton onClick={ () => setPopup(true) }>우편번호 검색</SearchButton>
             {popup && <DaumPostCode
                     onComplete={handleAddress}
                     autoClose
                     style={postCodeStyle}
                   />}
-          </InputField>
-          <InputField>
+          </InputWapper> 
+          <InputWapper>
             <InputTitle>주소</InputTitle>
             <Input value={address} type="text" placeholder="주소를 입력해주세요."/>
-          </InputField>
-          <InputField>
+          </InputWapper> 
+          <InputWapper>
             <InputTitle>상세주소</InputTitle>
             <Input type="text" placeholder="상세주소를 입력해주세요."/>
-          </InputField>
-        </InputList>
+          </InputWapper> 
+        </InputForm>
         <Button>등록</Button>
       </JoinWapper>
     </Container>
@@ -91,61 +130,126 @@ const Join = () => {
 }
 
 const Container = styled.section`
-font-family: 'Noto Sans KR', sans-serif;
-  width:500px;
+  width: 570px;
   border: 1px black solid;
   margin: 10px auto;
   padding: 10px 0px;
   text-align: center;
+  padding: 30px;
 `;
 
 const JoinWapper = styled.div`
   align-items: center;
 `;
 
-const AlertLine = styled.p`
- color:#9980FA;
- font-size: 5px;
-`;
-
-
-const InputTitle = styled.span`
-  margin-right:20px;
-`;
-
-
-const InputList = styled.ul`
-  width: 400px;
+const InputForm = styled.form`
+  width: 100%;
   list-style: none;
   display: flex;
   flex-wrap: wrap;
+  text-align: left;
+  margin: 0px 30px;
 `;
 
-const InputField = styled.li`
-  flex-direction: column;
-  margin: 10px 0px;
-  padding: 5px;
+const JoinTitle = styled.h1`
+  margin: 40px 0px;
+`;
 
+const InputWapper = styled.div`
+  width: 100%;
+  margin: 20px 0px;
+`;
+
+const AlertLine = styled.div`
+ color: red;
+ display: block;
+ width: 100%;
+ text-align: center;
+`;
+
+const InputTitle = styled.span`
+  position: absolute;
+  margin-right:20px;
+  width: 130px;
+  line-height: 55px;
+  font-size: 1.2em;
+
+`;
+
+const EmailInput = styled.input`
+  position: relative;
+  width: 280px;
+  height: 35px;
+  left: 110px;
+  padding-left: 5px;
+  font-size: 1.1em;
+`;
+
+const CheckButton = styled.button`
+  margin: auto;
+  background-color: transparent;
+  border: 1px #9980FA solid;
+  padding: 10px 10px;
+  color: #9980FA;
+  border-radius: 5px;
+  margin-left:15px;
+  left: 110px;
+  position: relative;
 `;
 
 const Input = styled.input`
-  width: 170px;
-  height: 25px;
+  position: relative;
+  width: 370px;
+  height: 35px;
   &:focus {
     outline: none;
   }
   padding-left: 5px;
+  left: 100px;
+  font-size: 1.1em;
+  margin: 0px 10px;
 `;
 
-const InnerButton = styled.button`
+const PWInput = styled.input`
+  position: relative;
+  width: 250px;
+  height: 35px;
+  &:focus {
+    outline: none;
+  }
+  padding-left: 5px;
+  margin: 0px 10px;
+  left: 100px;
+  font-size: 1.1em;
+`;
+
+const CheckPW = styled.span`
+  color: #9980FA;
+  position: relative;
+  left: 100px;
+`;
+
+const ZoneCodeInput = styled.input`
+  position: relative;
+  width: 250px;
+  height: 35px;
+  left: 110px;
+  font-size: 1.1em;
+  padding-left: 5px;
+`;
+
+
+const SearchButton = styled.button`
   margin: auto;
   background-color: transparent;
   border: 1px #9980FA solid;
-  padding: 5px 10px;
+  padding: 10px 10px;
   color: #9980FA;
   border-radius: 5px;
   margin-left:15px;
   margin-top:15px;
+  left: 110px;
+  position: relative;
 `;
 
 const Button = styled.button`
@@ -153,9 +257,10 @@ const Button = styled.button`
   font-size: 20px;
   background-color: #9980FA;
   border-radius: 5px;
-  padding: 5px 15px;
+  padding: 5px 35px;
   color: #fff;
   margin: auto;
+  margin: 30px 0px;
 `;
 
 export default Join;
