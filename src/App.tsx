631a9b2543d568abc8940513ced9467e9c8d8a3c
yoonyounghoon <yunyounghoon23@gmail.com>
2021-10-11 14:26:34 +0900

    feat: Add Login component, hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
 import GlobalStyle from './styles/globalStyles';
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Switch></Switch>
+      <Switch>
+        <Route path="login" component={LoginPage} />
+      </Switch>
     </BrowserRouter>
   );
 }
