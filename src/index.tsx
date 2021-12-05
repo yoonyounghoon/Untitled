e91517f02fcb4c4bff28101d8aacdd4958b27789
yoonyounghoon <yunyounghoon23@gmail.com>
2021-12-05 17:51:25 +0900

    feat: redux-persist 기본세팅, 새로고침 해도 스토어값 유지되게 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import '@fortawesome/fontawesome-free/js/all.js';
-import { store } from './modules/store';
+import { persistor, store } from './modules/store';
 import { Provider } from 'react-redux';
 import { SetUser } from './modules/Login/reducer';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function loadUser() {
   try {
@@ -21,7 +22,9 @@ loadUser();
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- src/modules/store.ts
+++ src/modules/store.ts
@@ -1,10 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { loginReducer } from '../modules/Login';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  login: loginReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    login: loginReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
